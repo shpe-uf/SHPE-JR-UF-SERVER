4ee9df685cc97cbc00475a80257c5e207ad81e11
MarianaTorresTorres <mari.torret@gmail.com>
2021-06-24 16:10:59 -0400

    add pubsub

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, PubSub } = require("apollo-server");
 const mongoose = require("mongoose");
 
 const typeDefs = require("./graphql/typeDefs.js");
@@ -6,10 +6,12 @@ const resolvers = require("./graphql/resolvers");
 
 require("dotenv").config();
 
+const pubsub = new PubSub();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req }),
+  context: ({ req }) => ({ req, pubsub }),
 });
 
 const port = process.env.PORT || 5000;
